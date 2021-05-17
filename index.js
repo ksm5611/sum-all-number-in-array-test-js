a6c3ed328c00a95b29a21c9a4a5dfb1e3b5ccf12
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-05-17 03:30:12 +0000

    iteration flow to sum nested array

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  let total = 0;
+  while (array.length > 0) {
+    const element = array.shift();
+
+    if (Array.isArray(element)) {
+      element.reverse().forEach((newElement) => {
+        array.unshift(newElement);
+      });
+    } else {
+      // console.log(typeof element)
+      total += element;
+    }
+  }
+  return total;
+};
+
+console.log(sumItems([1, 2, 3, 4, 5])); //15
+console.log(sumItems([[1, 2, [[3], 4]], 5, []])); //15
+console.log(sumItems([[[[[[[[[[[[[1]]]]]]]]]]]]])); //1
 
 module.exports = sumItems;
